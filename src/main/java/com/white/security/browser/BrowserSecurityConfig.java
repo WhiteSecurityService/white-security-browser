657f50aedabef6a2fa4a4409ff190e7495b36fc6
WhiteCode2016 <liukai20161219@outlook.com>
2018-09-10 17:06:58 +0800

    update

--- src/main/java/com/white/security/browser/BrowserSecurityConfig.java
+++ src/main/java/com/white/security/browser/BrowserSecurityConfig.java
@@ -1,9 +1,10 @@
 package com.white.security.browser;
 
+import com.white.security.core.authentication.FormAuthenticationConfig;
+import com.white.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
 import com.white.security.core.properties.SecurityConstants;
 import com.white.security.core.properties.SecurityProperties;
-import com.white.security.core.validate.ValidateCodeFilter;
-import com.white.security.core.validate.sms.SmsCodeFilter;
+import com.white.security.core.validate.ValidateCodeSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -50,6 +51,18 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private FormAuthenticationConfig formAuthenticationConfig;
+
+    @Autowired
+    private SpringSocialConfigurer whiteSocialSecurityConfig;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -65,37 +78,30 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
-        validateCodeFilter.setAuthenticationFailureHandler(whiteAuthenticationFailureHandler);
-        validateCodeFilter.setSecurityProperties(securityProperties);
-        validateCodeFilter.afterPropertiesSet();
 
-        SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
-        smsCodeFilter.setAuthenticationFailureHandler(whiteAuthenticationFailureHandler);
-        smsCodeFilter.setSecurityProperties(securityProperties);
-        smsCodeFilter.afterPropertiesSet();
+        // 表单配置
+        formAuthenticationConfig.configure(http);
 
-        http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin()
-                    .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
-                    .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
-                    .successHandler(whiteAuthenticationSuccessHandler)
-                    .failureHandler(whiteAuthenticationFailureHandler)
+        http.apply(validateCodeSecurityConfig)
                     .and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                    .and()
+                .apply(whiteSocialSecurityConfig)
+                    .and()
+                // 记住我配置
                 .rememberMe()
                     .tokenRepository(persistentTokenRepository())
                     .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
                     .userDetailsService(userDetailsService)
                     .and()
                 .authorizeRequests() // 定义哪些URL需要被保护、哪些不需要被保护
-                .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
-                        securityProperties.getBrowser().getLoginPage(),
-                        "/code/image",
-                        "/code/sms",
-                        "/authentication/mobile").permitAll()
-                .anyRequest().authenticated() // 任何请求,登录后可以访问
-                .and()
+                    .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                            SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                            securityProperties.getBrowser().getLoginPage(),
+                            SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*").permitAll()
+                    .anyRequest()
+                    .authenticated() // 任何请求,登录后可以访问
+                    .and()
                 .csrf().disable();
     }
 
